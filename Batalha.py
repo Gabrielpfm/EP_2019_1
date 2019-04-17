3ef1f2cd3a0640c80ef48b457d1a459bc8b43d69
Gabriel Pascua de Freitas Moreira <gabrielpfm@al.insper.edu.br>
2019-04-17 14:59:04 -0300

    deixou de ser função

--- Batalha.py
+++ Batalha.py
@@ -7,17 +7,18 @@ Created on Mon Apr 15 07:38:52 2019
 
 
 
-def batalhas(sistema_de_batalha,Health_bar):
-    if sistema_de_batalha == True:
-        print('Inicia Batalha')
-        vida_inimigo = 5
-        dano = 0
+
+if sistema_de_batalha == True:
+    print('Inicia Batalha')
+    vida_inimigo = 5
+    dano = 0
     while sistema_de_batalha == True :
+        dano = 0
         vida_inimigo = vida_inimigo - Quantidade_de_ataque(ataque)
         print ("Vida do seu Inimigo ==> {0}".format(vida_inimigo))
         if vida_inimigo <= 0:
             sistema_de_batalha = False
-            return ('Você venceu')
+            print ('Você venceu') 
         else:
             if Quantidade_de_defesa(defesa) > 0 :
                 dano += 1
@@ -25,11 +26,8 @@ def batalhas(sistema_de_batalha,Health_bar):
             else:
                 dano += 1 
                 dano_total += dano
-            if Health_bar(energia,dano,defesa) <= 0:
+            if Health_bar(energia,dano_total) <= 0:
                 print('Você perdeu a batalha')
                 sistema_de_batalha = False
-                game_over = True
-            print(Health_bar(energia,dano,defesa))
-            
-
+                print(Health_bar(energia,dano_total))
         
