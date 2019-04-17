13fb4f7ba045f3f5c274874bdd4bec8ceceee74f
Gabriel Pascua de Freitas Moreira <gabrielpfm@al.insper.edu.br>
2019-04-17 13:44:26 -0300

    Adicionando Mobs ao jogo

--- ep1.py
+++ ep1.py
@@ -4,6 +4,7 @@
 # - aluno A: Gabriel Pascua de Freitas Moreira, gabrielpfm@al.insper.edu.br
 # - aluno B: Gabriel Huerta Façanha, gabrielhf@al.insper.edu.br
 import json
+import random
 
 def carregar_cenarios():
     with open('cenarios.json','r',encoding = 'utf-8') as arquivo:
@@ -11,6 +12,8 @@ def carregar_cenarios():
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
 
+bem = {1 : {'um Ninja': 'ataque' } , 2 : {'um amigo': 'ataque'  } , 3 : {'um cachorro': 'defesa' } , 4 : {'uma marmita': 'defesa' } , 5: {'uma máquina de café':'energia'} }
+mal = {1: {'um veterano':{'Iniciar batalha':'batalha'}},2:{'um "amigo"':{'Ele copiou seu trabalho':"dano"}},3:{'uma nota ruim':{'Você ficou desanimado': 'defesa'}} ,4: {'um professor':{'Ele te lembrou de outro trabalho':'dano'}},5:{'uma goteira':{'Você se molhou e ficou lento':'ataque'}}}
 
 def main():
     print("Na hora do sufoco!")
@@ -41,6 +44,13 @@ def main():
         traco = '-' * len(nome_cenario_atual)
         return traco
     game_over = False
+    encontros = False
+    defesa = 0
+    energia = 0
+    ataque = 0
+    dano = 0
+    dano_total = 0
+        
     while not game_over:
         cenario_atual = cenarios[nome_cenario_atual]
         
@@ -50,9 +60,73 @@ def main():
         print (titulo)
         print (tracinho(titulo))
         print (descricao)
-        
+        print()
+        print("Stats")
+        print("Sua vida é {0}".format(Health_bar(energia,dano,defesa)))
+        print("Sua defesa é {0}".format(Quantidade_de_defesa(defesa)))
+        print("Seu ataque é {0}".format(Quantidade_de_ataque(ataque)))
+        dano = 0
+        if encontros == True:
+                MOBS = random.randint(1,11)
+                mob = random.randint(1,5)
+                encontro = random.randint(1,2)
+                if encontro == 2:
+                    if MOBS % 2 == 0:
+                        sorte = random.randint(1,2)
+                        quem = bem[mob]
+                        for nome in quem:
+                            pessoa = nome
+                            stat = quem[pessoa]
+                        encontro_bom = ('Ganhou {0} de {1}'.format(sorte,stat))
+                        print('Voce encontrou {0}'.format(pessoa))
+                        print()
+                        print ('*' * len(encontro_bom))
+                        print(encontro_bom)
+                        print ('*' * len(encontro_bom))
+                        if stat == "ataque":
+                            ataque += sorte
+                            
+                        elif stat == "defesa":
+                            defesa += sorte
+                            
+                        elif stat == "energia":
+                            energia += sorte
+                            
+                    else:
+                        azar = random.randint(1,2)
+                        quem = mal[mob]
+                        for nome in quem:
+                            pessoa = nome
+                            ação = quem[pessoa]
+                            for action in ação:
+                                oque = action
+                                stat = ação[oque]
+                            if stat == 'dano':
+                                
+                                dano += azar
+                                dano_total += dano
+                                encontro_ruim = ('Perdeu {0} de vida'.format(dano/Quantidade_de_defesa(defesa)))    
+                            else:        
+                                encontro_ruim = ('Perdeu {0} de {1}'.format(azar,stat))
+                        print ('Voce encontrou {0}'.format(pessoa))
+                        print(oque)
+                        if stat == 'batalha':
+                            sistema_de_batalha = True
+                            
+                        else:
+                            print()
+                            print('*'*len(encontro_ruim))
+                            print(encontro_ruim)
+                            print('*'*len(encontro_ruim))
+                            if stat == "ataque":
+                                ataque += (-azar)
+                                
+                            elif stat == "defesa":
+                                 defesa += (-azar)
+                                
+                    encontros = False
        
-    
+        encontros = True
         opcoes = cenario_atual['opcoes']
         if len(opcoes) == 0:
             print("Acabaram-se suas opções! Mwo mwo mwooooo...")
@@ -89,7 +163,7 @@ def main():
                 print()
             else:
                 print(" ----------------------------")
-                print(" |Sua indecisão foi sua ruína!")
+                print("Sua indecisão foi sua ruína!")
                 print(" ----------------------------")
                 game_over = True
 
