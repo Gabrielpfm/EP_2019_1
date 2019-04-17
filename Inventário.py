64564d2e414a16d8f0db0cf47d3a509895ca4f2e
Gabriel Pascua de Freitas Moreira <gabrielpfm@al.insper.edu.br>
2019-04-17 15:52:55 -0300

    Inventário

---
+++ Inventário.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 15:37:21 2019
+
+@author: gabri
+"""
+#dicionario_itens:
+inventario ={}
+if not item in inventario:
+    inventario[item] = 1
+else:
+    inventario[item] += 1
+    inventario = {}
